11afb4f8c8d716d03c3f227ae1d7756946d26bc6
Thomas Marks <tmsluigi@gmail.com>
2022-02-22 16:22:24 -0700

    Have legality of puzzles now tested

--- classes/puzzle.py
+++ classes/puzzle.py
@@ -3,6 +3,14 @@ from typing import Optional
 import numpy as np
 
 
+def is_legal_set(numbers: np.ndarray) -> bool:
+    numbers = numbers.flatten()
+    num_zeros = np.count_nonzero(numbers == 0)
+    number_set = set(numbers)
+    number_set.discard(0)
+    return num_zeros + len(number_set) == 9
+
+
 class Puzzle:
 
     def __init__(self, grid: Optional[np.ndarray] = None):
@@ -12,18 +20,46 @@ class Puzzle:
         contains values [0-9], otherwise an exception is raised
         """
         if grid is None:
-            self.grid = np.zeros((9, 9), dtype=np.int8)
+            self.puzzle_grid = np.zeros((9, 9), dtype=np.int8)
         else:
-            self.grid = grid
+            self.puzzle_grid = grid
         # Now make sure the grid is valid, and if not raise an exception
         if not self.is_puzzle_valid():
             raise ValueError("The input grid was not valid")
 
-    def __str__(self):
-        return str(self.grid)
+    def __str__(self) -> str:
+        return str(self.puzzle_grid)
 
-    def is_puzzle_empty(self):
-        return np.all(self.grid == 0)
+    def is_puzzle_empty(self) -> bool:
+        return np.all(self.puzzle_grid == 0)
 
-    def is_puzzle_valid(self):
-        return np.all((self.grid >= 0) & (self.grid <= 9)) and np.all(np.equal(self.grid.shape, (9, 9)))
+    def get_square(self, row, col) -> np.ndarray:
+        return self.puzzle_grid[row * 3:row * 3 + 3, col * 3:col * 3 + 3]
+
+    def is_puzzle_valid(self) -> bool:
+        # Check only has legal values
+        if not np.all((self.puzzle_grid >= 0) & (self.puzzle_grid <= 9)):
+            return False
+        # Check has the right shape
+        if not np.all(np.equal(self.puzzle_grid.shape, (9, 9))):
+            return False
+        # Check all squares legal
+        for row in range(3):
+            for col in range(3):
+                if not is_legal_set(self.get_square(row, col)):
+                    return False
+        # Check all rows legal
+        for row in range(9):
+            if not is_legal_set(self.puzzle_grid[row, :]):
+                return False
+        # Check all cols legal
+        for col in range(9):
+            if not is_legal_set(self.puzzle_grid[:, col]):
+                return False
+
+        # Passed all checks so it is valid
+        return True
+
+    @classmethod
+    def make_solvable_puzzle(cls):
+        pass
--- tests/test_classes/test_puzzle.py
+++ tests/test_classes/test_puzzle.py
@@ -7,7 +7,7 @@ from classes.puzzle import Puzzle
 def test_empty_puzzle_constructor():
     puzzle = Puzzle()
     # Ensure that it's the right shape
-    assert puzzle.grid.shape == (9, 9)
+    assert puzzle.puzzle_grid.shape == (9, 9)
     # Ensure it's empty
     assert puzzle.is_puzzle_empty()
     assert puzzle.is_puzzle_valid()
@@ -17,7 +17,7 @@ def test_grid_provided_constructor_happy():
     input_grid = np.zeros((9, 9), dtype=np.int8)
     input_grid[0, 2] = 5
     good_puzzle = Puzzle(input_grid)
-    assert np.all(np.equal(good_puzzle.grid, input_grid))
+    assert np.all(np.equal(good_puzzle.puzzle_grid, input_grid))
     assert not good_puzzle.is_puzzle_empty()
     assert good_puzzle.is_puzzle_valid()
 
@@ -29,7 +29,37 @@ def test_grid_provided_wrong_size():
 
 
 def test_grid_provided_invalid_input_data():
+    # Wrong number
     input_grid = np.zeros((9, 9), dtype=np.int8)
     input_grid[0, 2] = 50
     with pytest.raises(ValueError):
         Puzzle(input_grid)
+
+
+def test_grid_provided_invalid_square():
+    # Illegal square
+    input_grid = np.zeros((9, 9), dtype=np.int8)
+    input_grid[0, 2] = 5
+    input_grid[1, 1] = 5
+    with pytest.raises(ValueError):
+        Puzzle(input_grid)
+
+
+def test_grid_provided_invalid_row():
+    input_grid = np.zeros((9, 9), dtype=np.int8)
+    input_grid[1, 2] = 5
+    input_grid[1, 8] = 5
+    with pytest.raises(ValueError):
+        Puzzle(input_grid)
+
+
+def test_grid_provided_invalid_col():
+    input_grid = np.zeros((9, 9), dtype=np.int8)
+    input_grid[0, 1] = 5
+    input_grid[8, 1] = 5
+    with pytest.raises(ValueError):
+        Puzzle(input_grid)
+
+
+def test_puzzle_generation():
+    puzzle = Puzzle.make_solvable_puzzle()

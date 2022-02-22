c86c43263775dd5555991df380f3c94baa1e0e08
Thomas Marks <tmsluigi@gmail.com>
2022-02-22 15:28:28 -0700

    Puzzle constructor in now

--- classes/puzzle.py
+++ classes/puzzle.py
@@ -8,10 +8,22 @@ class Puzzle:
     def __init__(self, grid: Optional[np.ndarray] = None):
         """
         Makes a puzzle grid
-        :param grid: A grid already holding some values. If passed this will be used as the grid if a correct size,
-        otherwise an exception is raised
+        :param grid: A grid already holding some values. If passed this will be used as the grid if a correct size and
+        contains values [0-9], otherwise an exception is raised
         """
-        self.grid = np.zeros((9, 9), dtype=np.int8)
+        if grid is None:
+            self.grid = np.zeros((9, 9), dtype=np.int8)
+        else:
+            self.grid = grid
+        # Now make sure the grid is valid, and if not raise an exception
+        if not self.is_puzzle_valid():
+            raise ValueError("The input grid was not valid")
 
     def __str__(self):
         return str(self.grid)
+
+    def is_puzzle_empty(self):
+        return np.all(self.grid == 0)
+
+    def is_puzzle_valid(self):
+        return np.all((self.grid >= 0) & (self.grid <= 9)) and np.all(np.equal(self.grid.shape, (9, 9)))
--- tests/test_classes/test_puzzle.py
+++ tests/test_classes/test_puzzle.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pytest
+
 from classes.puzzle import Puzzle
 
 
@@ -6,8 +9,27 @@ def test_empty_puzzle_constructor():
     # Ensure that it's the right shape
     assert puzzle.grid.shape == (9, 9)
     # Ensure it's empty
-    assert puzzle.grid.sum() == 0
+    assert puzzle.is_puzzle_empty()
+    assert puzzle.is_puzzle_valid()
 
 
-def test_grid_provided_constructor():
-    pass
+def test_grid_provided_constructor_happy():
+    input_grid = np.zeros((9, 9), dtype=np.int8)
+    input_grid[0, 2] = 5
+    good_puzzle = Puzzle(input_grid)
+    assert np.all(np.equal(good_puzzle.grid, input_grid))
+    assert not good_puzzle.is_puzzle_empty()
+    assert good_puzzle.is_puzzle_valid()
+
+
+def test_grid_provided_wrong_size():
+    input_grid = np.zeros((16, 16), dtype=np.int8)
+    with pytest.raises(ValueError):
+        Puzzle(input_grid)
+
+
+def test_grid_provided_invalid_input_data():
+    input_grid = np.zeros((9, 9), dtype=np.int8)
+    input_grid[0, 2] = 50
+    with pytest.raises(ValueError):
+        Puzzle(input_grid)
